a9579aac0d1d38470db700d371bea73bf0a0fbdb
Vahid Nejad <vahid884@gmail.com>
2024-12-27 11:55:21 +0330

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import dbConfig from './config/db.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
